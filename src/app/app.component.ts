38431dcc9ca5bdaec97ba17165e0076bc2dcfbab
Manar-Antar <manar.antar@eden-tech.io>
2023-09-26 12:58:46 +0300

    convert project into standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MainComponentComponent } from './main-component/main-component.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet,MainComponentComponent]
 })
 export class AppComponent {
   title = 'newProject';
---
+++ src/app/main-component/main-component.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-component.component.html',
+  styleUrls: ['./main-component.component.scss']
+})
+export class MainComponentComponent {
+
+}
